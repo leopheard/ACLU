f0520ce89e03bcfb46397851256f5da23f334790
leopheard <leopheard.misc@gmail.com>
2019-10-21 01:14:29 -0400

    uploads

--- resources/lib/mainaddon.py
+++ resources/lib/mainaddon.py
@@ -3,20 +3,22 @@ import re
 from bs4 import BeautifulSoup
 
 def get_soup(url):
-    """
-    @param: url of site to be scraped
-    """
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
-    print "type: ", type(soup)
+    print("type: ", type(soup1))
     return soup
-get_soup("https://feeds.soundcloud.com/users/soundcloud:users:40330678/sounds.rss")
-
+def get_soup2(url2):
+    page = requests.get(url2)
+    soup2 = BeautifulSoup(page.text, 'html.parser')
+    print("type: ", type(soup2))
+    return soup2
+def get_soup3(url3):
+    page = requests.get(url3)
+    soup3 = BeautifulSoup(page.text, 'html.parser')
+    print("type: ", type(soup3))
+    return soup3
 
 def get_playable_podcast(soup):
-    """
-    @param: parsed html page            
-    """
     subjects = []
     for content in soup.find_all('item'):
         try:        
@@ -39,13 +41,8 @@ def get_playable_podcast(soup):
         }
         subjects.append(item) 
     return subjects
-
 def compile_playable_podcast(playable_podcast):
-    """
-    @para: list containing dict of key/values pairs for playable podcasts
-    """
     items = []
-
     for podcast in playable_podcast:
         items.append({
             'label': podcast['title'],
@@ -56,14 +53,10 @@ def compile_playable_podcast(playable_podcast):
     })
     return items
 
-def get_playable_podcast1(soup):
-    """
-    @param: parsed html page            
-    """
+def get_playable_podcast2(soup2):
     subjects = []
-
-    for content in soup.find_all('item', limit=7):
-        try:        
+    for content in soup.find_all('item'):
+        try:
             link = content.find('enclosure')
             link = link.get('url')
             print("\n\nLink: ", link)
@@ -79,18 +72,13 @@ def get_playable_podcast1(soup):
                 'url': link,
                 'title': title,
 #                'desc': desc,
-                'thumbnail': "https://www.aclu.org/files/podcast/podcast-at-liberty-landing-page-cover.jpg",
+                'thumbnail': "https://i1.sndcdn.com/avatars-000427832679-g12u3l-original.jpg",
         }
         subjects.append(item) 
     return subjects
-
-def compile_playable_podcast1(playable_podcast1):
-    """
-    @para: list containing dict of key/values pairs for playable podcasts
-    """
+def compile_playable_podcast2(playable_podcast2):
     items = []
-
-    for podcast in playable_podcast1:
+    for podcast in playable_podcast2:
         items.append({
             'label': podcast['title'],
             'thumbnail': podcast['thumbnail'],
@@ -98,5 +86,39 @@ def compile_playable_podcast1(playable_podcast1):
 #            'info': podcast['desc'],
             'is_playable': True,
     })
+    return items
 
+def get_playable_podcast3(soup3):
+    subjects = []
+    for content in soup3.find_all('item'):
+        try:
+            link = content.find('enclosure')
+            link = link.get('url')
+            print("\n\nLink: ", link)
+            title = content.find('title')
+            title = title.get_text()
+#            desc = content.find('description')
+#            desc = desc.get_text()
+#            thumbnail = content.find('itunes:image')
+#            thumbnail = desc.get_text('href')
+        except AttributeError:
+            continue
+        item = {
+                'url': link,
+                'title': title,
+#                'desc': desc,
+                'thumbnail': "https://static.libsyn.com/p/assets/a/3/6/5/a36538cb1437928e/clm_cover_600x600.jpg",
+        }
+        subjects.append(item) 
+    return subjects
+def compile_playable_podcast3(playable_podcast3):
+    items = []
+    for podcast in playable_podcast3:
+        items.append({
+            'label': podcast['title'],
+            'thumbnail': podcast['thumbnail'],
+            'path': podcast['url'],
+#            'info': podcast['desc'],
+            'is_playable': True,
+    })
     return items
